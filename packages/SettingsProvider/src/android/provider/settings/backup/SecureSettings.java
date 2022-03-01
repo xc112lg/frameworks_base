821e8179c99a5f7537e30ff2ff74ebbcd3fac85e
cjh1249131356 <cjh1249131356@gmail.com>
2022-03-01 18:06:59 +0800

    base: Allow to skip confirmation in biometric auth dialog [1/2]
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I23f414b341a309b32b378503fe58d7ba11b0e6f4

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -247,5 +247,6 @@ public class SecureSettings {
         Settings.Secure.DOZE_ON_CHARGE,
         Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE,
         Settings.Secure.NFC_SOUNDS,
+        Settings.Secure.IGNORE_AUTH_CONFIRMATION,
     };
 }
