586b3da177c59ca76076e2985b9ba43e655293e1
Joey Huab <joey@evolution-x.org>
2023-12-10 00:14:58 +0900

    [TMP][SQUASH] Drop Pulse for Android 14 QPR1
    Revert "Pulse: Simplify checks and prevent issues"
    
    This reverts commit 9f179ea5116413a14c63c8d3799130bed88c4202.
    
    Revert "ColorContoller: Fix getting default accent color"
    
    This reverts commit 85c5a4df91a71161bcc22eb27ade4d2f30a07ac7.
    
    Revert "Pulse: Implement pulse color based album art [1/2]"
    
    This reverts commit e7cffb3d204abd27b9687a93cbaf07ddea58bf35.
    
    Revert "Pulse: Detach pulse view only when attached"
    
    This reverts commit a34dd775bd6b6cd13fc0247af7dd08828cb01ac4.
    
    Revert "Pulse: Solid renderer round lines [1/2]"
    
    This reverts commit d257c8e6670465d997da83483f1ba03119e6c488.
    
    Revert "Pulse: Set current Refresh rate as FPS animation value"
    
    This reverts commit c11758525c999d31337cc78ee930a2bf9d6e57c8.
    
    Revert "Pulse: Extend to Ambient Screen"
    
    This reverts commit 8dae0f7608d074f7ffdc08d95f7516a5f6741418.
    
    Revert "Pulse initial checkin for Android 14 [1/2]"
    
    This reverts commit 93e7049dbaa71c5532ada249d791b14444cad63c.

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -357,12 +357,9 @@ public class NotificationMediaManager implements Dumpable {
         mMediaListeners.add(callback);
         callback.onPrimaryMetadataOrStateChanged(mMediaMetadata,
                 getMediaControllerPlaybackState(mMediaController));
-        callback.setMediaNotificationColor(mColorExtractor.getMediaBackgroundColor());
     }
 
     public void removeCallback(MediaListener callback) {
-        mColorExtractor.setMediaBackgroundColor(0);
-        callback.setMediaNotificationColor(0);
         mMediaListeners.remove(callback);
     }
 
@@ -465,7 +462,6 @@ public class NotificationMediaManager implements Dumpable {
         ArrayList<MediaListener> callbacks = new ArrayList<>(mMediaListeners);
         for (int i = 0; i < callbacks.size(); i++) {
             callbacks.get(i).onPrimaryMetadataOrStateChanged(mMediaMetadata, state);
-            callbacks.get(i).setMediaNotificationColor(mColorExtractor.getMediaBackgroundColor());
         }
     }
 
@@ -874,7 +870,5 @@ public class NotificationMediaManager implements Dumpable {
          */
         default void onPrimaryMetadataOrStateChanged(MediaMetadata metadata,
                 @PlaybackState.State int state) {}
-
-        default void setMediaNotificationColor(int color) {};
     }
 }
